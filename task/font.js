031619f82a3d0e9c2e7a07ce83458a7a3ca8bfd2
Lofty777 <meshkov88@gmail.com>
2022-09-15 13:31:50 +0700

    changed font.js add gulp-ttf2woff

--- task/font.js
+++ task/font.js
@@ -10,19 +10,23 @@ const notify = require("gulp-notify");
 const newer = require("gulp-newer");
 const fonter = require("gulp-fonter");
 const ttf2woff2 = require("gulp-ttf2woff2");
+const ttf2woff = require("gulp-ttf2woff");
 
 
 // Обработка Font
 const font = () => {
-	return src(path.font.src)
-		.pipe(plumber({
-			errorHandler: notify.onError()
-		}))
-		.pipe(newer(path.font.dest))
-		//.pipe(fonter(app.fonter))
-		.pipe(dest("./public/font/"))
-		.pipe(ttf2woff2())
-		.pipe(dest(path.font.dest));
+    src(path.font.src)
+        .pipe(
+            plumber({
+                errorHandler: notify.onError()
+            })
+        )
+        .pipe(newer(path.font.dest))
+        //.pipe(fonter(app.fonter))
+        .pipe(dest(path.font.dest))
+        .pipe(ttf2woff2())
+        .pipe(dest(path.font.dest))
+    return src(path.font.src).pipe(ttf2woff()).pipe(dest(path.font.dest))
 }
 
 module.exports = font;
